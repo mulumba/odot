136fafd93bf00b4522cc5132a37f37020a1ad3dc
Mulumba Lwatula <mulumba@mn8.co>
2014-07-26 23:08:43 -0400

    Added viewing lists feature

--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -1,10 +1,3 @@
-# require 'rails_helper'
-
-# RSpec.describe TodoItem, :type => :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
-
-
 require 'spec_helper'
 
 describe TodoItem do
